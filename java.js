5c42b40a612cbec5c4a75a55ba5551072d5fd1a6
matiasfelipew <matiaswhiat@gmail.com>
2024-11-07 22:50:22 -0300

    commit

--- java.js
+++ java.js
@@ -1,11 +1,8 @@
-// Esperar a que todo el contenido de la página esté cargado
 window.addEventListener('load', function() {
-    // Seleccionamos el video del preloader
-    const preloaderVideo = document.getElementById('preloader-video');
-    
-    // Esperamos 4 segundos (4000 ms) después de que el video haya comenzado
+    // Espera 4 segundos antes de ocultar el preloader
     setTimeout(function() {
         // Añadir la clase 'loaded' al body cuando haya pasado el tiempo
         document.body.classList.add('loaded');
-    }, 4000); // Tiempo de retraso de 4 segundos
+    }, 4000); // Retraso de 4 segundos
 });
+
